4ff94613642252e6c255bbec142d9af1032ffe9c
Manali Tijare <manali.tijare11@gmail.com>
2021-04-11 21:49:45 -0400

    react web app

---
+++ src/components/Cards.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Cards.css";
+import CardItem from "./CardItem";
+
+function Cards() {
+  return (
+    <div className="cards">
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src="images/img-9.jpg"
+              text="Explore the paris"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Explore the beauty of India "
+              label="Luxury"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/img-3.jpg"
+              text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+              label="Mystery"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-8.jpg"
+              text="Ride through the Sahara Desert on a guided camel tour"
+              label="Adrenaline"
+              path="/sign-up"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
